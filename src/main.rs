111282ccb94f26c9ec82d7e326bb0af84f8f6f88
Renell Castro <renellc9819@gmail.com>
2020-08-11 15:49:00 -0700

    Refactored to be simpler to understand
    - Previously, a mutex held the entire vector of forks. While this worked, it involved each philosopher to loop and reacquire this lock if they could not acquire a fork. Thus it was switched to a vector that holds n amount of mutexes (that contain nothing). Because the size of the vector never changes, its safe to access the table concurrently and we only acquire attempt to acquire the lock for the fork we need.

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,40 @@ use std::time::Duration;
 const DEFAULT_NUM_PHILOSOPHERS: usize = 5;
 const SIMULATION_TIME: u64 = 60 /* seconds */ * 3;
 
-#[derive(Debug, Copy)]
-enum Fork {
-    Free,
-    Occupied(usize),
-}
-
-impl Clone for Fork {
-    fn clone(&self) -> Self {
-        match self {
-            Fork::Free => Fork::Free,
-            Fork::Occupied(philosopher) => Fork::Occupied(*philosopher),
-        }
-    }
-}
-
 /// A philosopher picks up the specified fork on the table.
-fn pickup_fork(table: &Arc<Mutex<Vec<Fork>>>, philosopher: usize, fork: usize) {
-    loop {
-        let mut forks = table.lock().unwrap();
-        if let Fork::Free = forks[fork] {
-            forks[fork] = Fork::Occupied(philosopher);
-            break;
-        }
-    }
-}
+fn eat(table: &Arc<Vec<Mutex<()>>>, philosopher: usize, eat_time: u64) {
+    let table = table.clone();
+    let len = table.len();
+    // Here the philosopher will pick up their forks one at a time.
+    // If each philosopher picks up the left fork first, a deadlock occur because each philosopher will be
+    // waiting for their respective right fork to be available. Since all forks are taken, no progress will
+    // be made, hence the deadlock.
+    // To avoid this problem, every other philosopher will pick up the left fork first then the right while
+    // the rest will pick up the right fork first. This avoids the deadlock as the worst possible scenario
+    // only one philosopher will be eating. Since the philosopher won't be eating forever, we will
+    // eventually enter a state that allows the neighboring philosophers to pick up their missing fork.
 
-/// A philosopher drops both of their forks on the table.
-fn drop_forks(table: &Arc<Mutex<Vec<Fork>>>, philosopher: usize) {
-    let mut forks = table.lock().unwrap();
-    let forks_len = forks.len();
-    forks[(philosopher + forks_len) % forks_len] = Fork::Free;
-    forks[(philosopher + forks_len + 1) % forks_len] = Fork::Free;
+    // This is translated to every even numberd philosopher will pick up their left fork first and every odd
+    // numbered philosopher will pick up their right fork first.
+    let first_fork = if philosopher % 2 == 0 {
+        (philosopher + len) % len
+    } else {
+        (philosopher + len + 1) % len
+    };
+
+    let second_fork = if philosopher % 2 == 0 {
+        (philosopher + len + 1) % len
+    } else {
+        (philosopher + len) % len
+    };
+
+    let _first_fork = table[first_fork].lock().unwrap();
+    let _second_fork = table[second_fork].lock().unwrap();
+    println!(
+        "Philosopher {} currently eating for {} seconds",
+        philosopher, eat_time
+    );
+    thread::sleep(Duration::from_secs(eat_time as u64));
 }
 
 fn main() {
@@ -54,8 +56,13 @@ fn main() {
         })
         .unwrap();
 
-    // The number of chopsticks is always equal to the number of philosophers.
-    let table = Arc::new(Mutex::new(vec![Fork::Free; num_philosophers]));
+    // The number of forks is always equal to the number of philosophers.
+    let mut forks = Vec::<Mutex<()>>::with_capacity(num_philosophers);
+    for _ in 0..num_philosophers {
+        forks.push(Mutex::new(()));
+    }
+
+    let table = Arc::new(forks);
     for phil in 0..num_philosophers {
         let table = table.clone();
         thread::spawn(move || {
@@ -73,49 +80,14 @@ fn main() {
 
                 // Likewise, a philosopher is allowed to eat for a maximum of 32 seconds.
                 let eat_time = (rng.gen::<u8>() >> 3) + 1;
-
-                // Here the philosopher will pick up their forks one at a time.
-                // If each philosopher picks up the left fork first, a deadlock occur because each philosopher will be
-                // waiting for their respective right fork to be available. Since all forks are taken, no progress will
-                // be made, hence the deadlock.
-                // To avoid this problem, every other philosopher will pick up the left fork first then the right while
-                // the rest will pick up the right fork first. This avoids the deadlock as the worst possible scenario
-                // only one philosopher will be eating. Since the philosopher won't be eating forever, we will
-                // eventually enter a state that allows the neighboring philosophers to pick up their missing fork.
-
-                // This is translated to every even numberd philosopher will pick up their left fork first and every odd
-                // numbered philosopher will pick up their right fork first.
-                let first_chopstick = if phil % 2 == 0 {
-                    (phil + num_philosophers) % num_philosophers
-                } else {
-                    (phil + num_philosophers + 1) % num_philosophers
-                };
-
-                let second_chopstick = if phil % 2 == 0 {
-                    (phil + num_philosophers + 1) % num_philosophers
-                } else {
-                    (phil + num_philosophers) % num_philosophers
-                };
-
                 println!("Philosopher {} preparing to eat...", phil);
-                // println!("Philosopher {} trying to pick up fork {}", phil, first_chopstick);
-                pickup_fork(&table, phil, first_chopstick);
-
-                // println!("Philosopher {} trying to pick up fork {}", phil, second_chopstick);
-                pickup_fork(&table, phil, second_chopstick);
-                println!(
-                    "Philosopher {} currently eating for {} seconds",
-                    phil, eat_time
-                );
-                thread::sleep(Duration::from_secs(eat_time as u64));
+                eat(&table, phil, eat_time as u64);
 
                 println!("Philosopher {} done eating", phil);
                 println!();
-                drop_forks(&table, phil);
                 // thread::sleep(Duration::from_millis(1));
             }
         });
     }
-
     thread::sleep(Duration::from_secs(SIMULATION_TIME));
 }
